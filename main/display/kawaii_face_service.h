a6bd89b35bdee86d090d8e4b67aa2bc31a4854c3
ProjectMurzik <deadmiracle@gmail.com>
2026-06-10 18:18:15 +0300

    Update kawaii_face_service.h

--- main/display/kawaii_face_service.h
+++ main/display/kawaii_face_service.h
@@ -12,6 +12,13 @@ extern "C" {
  */
 void kawaii_face_service_init(lv_obj_t *parent);
 
+/**
+ * @brief Установка эмоции по имени события
+ * @param event_name Имя события ("love", "happy", "thinking" и т.д.)
+ * @note Потокобезопасна, можно вызывать из любого потока
+ */
+void kawaii_face_set_emotion(const char *event_name);
+
 #ifdef __cplusplus
 }
 #endif
